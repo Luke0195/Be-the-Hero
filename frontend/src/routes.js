87ee0552393f831ffd95b83d6826581edb97e16f
Lucas Alves Dos Santos <lucas201273@gmail.com>
2020-03-25 19:37:08 -0300

    Criação e estilização do componente Profile

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Logon  from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 function Routes(){
   return(
@@ -10,6 +11,7 @@ function Routes(){
       <Switch>
         <Route path="/" exact component={Logon}/>   {/*A rota principal da aplicação recebera o componente logon, a  propriedade exact não ira diferenciar as  rotas */}
         <Route path="/register" component={Register}/>
+        <Route path="/profile" component={Profile}/>
       </Switch>
      </BrowserRouter>
   )
