04976011f382174dcc07d107a97a363bb812d701
Lucas Alves Dos Santos <lucas201273@gmail.com>
2020-03-25 22:31:15 -0300

    Finalização da aplicação Web

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Logon  from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 function Routes(){
   return(
@@ -12,6 +13,7 @@ function Routes(){
         <Route path="/" exact component={Logon}/>   {/*A rota principal da aplicação recebera o componente logon, a  propriedade exact não ira diferenciar as  rotas */}
         <Route path="/register" component={Register}/>
         <Route path="/profile" component={Profile}/>
+        <Route path="/incidents/new" component={NewIncident}/>
       </Switch>
      </BrowserRouter>
   )
